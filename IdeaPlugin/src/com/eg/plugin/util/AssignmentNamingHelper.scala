b62a5ee6e568013b15771588bb2b5aa8f8102bcd
Evgeny Kurnevsky <kurnevsky@gmail.com>
2020-01-20 13:40:17 +0300

    Add shortestpath assignment.

--- IdeaPlugin/src/com/eg/plugin/util/AssignmentNamingHelper.scala
+++ IdeaPlugin/src/com/eg/plugin/util/AssignmentNamingHelper.scala
@@ -10,6 +10,28 @@ object AssignmentNamingHelper {
       "Calculator",
       "Simple task to implement the calculator"
     ),
+    AssignmentDescription(
+      "shortestpath",
+      "Shortest path",
+      """
+      |Find a path in the provided graph which visits all vertices (and returns to the initial vertex, thus it is circular).
+      |
+      |You solution should attempt to develop a solution which calculates as short a path as possible (even if not necessarily the shortest possible), given the travel times as input.
+      |
+      |Assume the following classes are provided:
+      |
+      |package com.evolutiongaming.conferences.graph
+      |
+      |final case class Vertex(id: String) extends AnyVal
+      |final case class Edge(from: Vertex, to: Vertex, cost: Double)
+      |
+      |The path will always exist in the test cases that your solution will be invoked upon.
+      |
+      |The travel possibilities are expressed as a set of Edge-s, and the shortest path should be returned as a List[Vertex]. The first and last vertices should be identical in the resulting List.
+      |
+      |The solution will be graded on test cases with vertex counts up to 100 and edge counts up to 9,900 and the total execution time of 10 test cases of various size constrained to 100 seconds.
+      """.stripMargin
+    ),
   )
 
   def getProjectPath(projectName: String) =
